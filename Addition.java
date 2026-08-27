122732eb71bf2312ca7b634cd42b7344de947fce
velagaleti14 <venumadhav.velagaleti@gmail.com>
2026-08-28 03:26:37 +0530

    addition

---
+++ Addition.java
@@ -0,0 +1,10 @@
+package DSA;
+
+public class Addition {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int c = a + b;
+        System.out.println("The sum of " + a + " and " + b + " is " + c);
+    }
+}
